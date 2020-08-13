a7ab28ab0e5880f6efc30276185a0c8f8debecd5
Ilham Rahman <ilhaamrahman@gmail.com>
2020-08-13 12:49:12 +0700

    setting up cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: %i[get post patch put delete]
+    origins 'http://localhost:3000'
+    resource '*', headers: :any, methods: %i[get post patch put delete options head], credentials: true
+  end
+
+  allow do
+    origins 'https://my-task-manager.netlify.app'
+    resource '*', headers: :any, methods: %i[get post patch put delete options head], credentials: true
   end
 end
