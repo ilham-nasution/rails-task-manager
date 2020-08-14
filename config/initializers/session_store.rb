61356c2a560957700dbb9fb3913e96141c5ffdb1
Ilham Rahman <ilhaamrahman@gmail.com>
2020-08-14 10:21:16 +0700

    try to fix cookie again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_todo_app', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_todo_app', domain: '.glacial-taiga-73174.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_todo_app'
 end
