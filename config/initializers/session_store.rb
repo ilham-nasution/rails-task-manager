d377c47f3b00c31c17a9715e08b18bf5f568d9b7
Ilham Rahman <ilhaamrahman@gmail.com>
2020-08-14 12:21:49 +0700

    try to fix cookie same-site again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_todo_app', domain: 'limitless-headland-79091.herokuapp.com', same_site: :none
+  Rails.application.config.session_store :cookie_store, key: '_todo_app', domain: 'limitless-headland-79091.herokuapp.com', same_site: :none, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_todo_app'
 end
